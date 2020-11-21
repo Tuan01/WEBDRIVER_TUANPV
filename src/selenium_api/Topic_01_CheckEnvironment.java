4e416816e5a089f895845e1f4848a85ed0b29deb
Pham Van Tuan <tuanpv92@gmail.com>
2020-11-21 15:54:10 +0700

    Add first

---
+++ src/selenium_api/Topic_01_CheckEnvironment.java
@@ -0,0 +1,43 @@
+package selenium_api;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Topic_01_CheckEnvironment {
+	
+	WebDriver driver;
+	
+  @Test
+  public void TC_01_CheckEnvironment() {
+	  String homePageTitle = driver.getTitle();
+	  Assert.assertEquals(homePageTitle,"Guru99 Bank Home Page");
+	  
+	  String homePageUrl = driver.getCurrentUrl();
+	  Assert.assertEquals(homePageUrl, "http://demo.guru99.com/V4/");
+	  	
+	  
+  }
+  @BeforeClass
+  public void beforeClass() {
+//	 driver = new FirefoxDriver();
+	 driver = new ChromeDriver();
+	 driver.get("http://demo.guru99.com/V4/");
+	 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	 driver.manage().window().maximize();
+	 
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
